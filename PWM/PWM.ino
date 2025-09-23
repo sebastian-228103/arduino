b33e0946534de358e426e88ddeec7cfda691e757
sebastian <sebastian.pradenas05@inacapmail.cl>
2025-09-23 04:27:08 -0300

    test n°q
    test

---
+++ PWM/PWM.ino
@@ -0,0 +1,23 @@
+const int RPWM = 25;
+const int LPWM = 26;
+
+int velocidad = 200;
+
+void setup() {
+  pinMode(RPWM, OUTPUT);
+  pinMode(LPWM, OUTPUT);
+}
+
+void loop() {
+  analogWrite(RPWM, velocidad); 
+  analogWrite(LPWM, 0);         
+  delay(2000);
+
+  analogWrite(RPWM, 0);
+  analogWrite(LPWM, velocidad);
+  delay(2000);
+
+  analogWrite(RPWM, 0);
+  analogWrite(LPWM, 0);
+  delay(2000);
+}
